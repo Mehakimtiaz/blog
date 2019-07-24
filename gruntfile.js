181eda7d1095e0b3ab98a15fc31094bc6b1c2545
Mehakimtiaz <42845528+Mehakimtiaz@users.noreply.github.com>
2019-07-24 09:49:01 +0500

    Add files via upload

--- gruntfile.js
+++ gruntfile.js
@@ -8,25 +8,49 @@ grunt.initConfig({
             dest:'build/styles.css'
         }
     },
+    uglify: {
+        my_target: {
+          files: {
+            'dest/output.min.js': ['src/js/index.js', 'src/js/contact_me.js']
+          }
+        }
+      },
+      copy: {
+        main: {
+          expand: true,
+          src: 'src/*',
+          dest: 'dest2/',
+        },
+      },
     cssmin: {
       target: {
         files: [{
             expand: true,
-            cwd: '<%= buildDistribution %>',
-            src: ['**/*.css'],
-            dest: '<%= buildDistribution %>'
+            src:['src/css/*.css'],
+            dest:'build/styles2.css'
         }]
     }
+      },
+      clean: {
+        build: {
+          src: ['build/styles.css', 'dest/output.min.js']
+        }
       }
 });
 //load plugins
 grunt.loadNpmTasks('grunt-contrib-concat');
 grunt.loadNpmTasks('grunt-contrib-cssmin');
+grunt.loadNpmTasks('grunt-contrib-uglify');
+grunt.loadNpmTasks('grunt-contrib-clean');
+grunt.loadNpmTasks('grunt-contrib-copy');
 
+grunt.registerTask('clean-build',['clean:build']);
+grunt.registerTask('copy-main',['copy:main']);
 grunt.registerTask('concat-css',['concat:css']);
-grunt.registerTask('cssmin-css',['cssmin:css']);
-grunt.registerTask('all',['concat-css','cssmin-css']);
+grunt.registerTask('cssmin-css',['cssmin:target']);
+grunt.registerTask('uglify-js',['uglify:my_target']);
+grunt.registerTask('all',['concat-css','uglify-js','cssmin-css','copy-main']);
 //regiter tasks
-//regiter tasks
+
 
 };
